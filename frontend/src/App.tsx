98aced4855fb7c2c5886bb9d7959c621296d4a40
Harriet Thompson <46738131+hazthompson@users.noreply.github.com>
2021-06-09 13:39:10 +0100

    Add proxy to react-app to point to backend
    Console log hardcoded backend data at GET /api frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  useEffect(()=> {
+
+    fetch('/api')
+      .then(response => response.json())
+      .then(data => console.log(data));
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
