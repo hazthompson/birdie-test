7a7a6664654ad72cf2b63850add1221e7fcc4d99
Harriet Thompson <46738131+hazthompson@users.noreply.github.com>
2021-06-11 15:10:15 +0100

    Add further scriptst and further info needed for Heroku deploy

--- backend/src/application.ts
+++ backend/src/application.ts
@@ -1,8 +1,17 @@
 import * as express from "express";
 import { eventsController } from "./controllers/events";
-
+// eslint-disable-next-line
+const path = require("path");
 const app = express();
 
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, "../../frontend/build")));
+
 app.use("/api", eventsController);
+
+// All other GET requests not handled before will return our React app
+app.get("*", (_req, res) => {
+  res.sendFile(path.resolve(__dirname, "../../frontend/build", "index.html"));
+});
 
 export default app;
